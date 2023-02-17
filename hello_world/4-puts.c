c24e9d865ddeb8d23b97c1e8449887504c4b1b90
Ericka76 <erickacarmella8@gmail.com>
2023-02-16 18:58:51 -0800

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /** 
- * main - Entry
- * Return: Always 0
+ * main - Prints 
+ * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
