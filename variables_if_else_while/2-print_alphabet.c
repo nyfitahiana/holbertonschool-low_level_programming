3669a1ef882d9833e68e8fe25a880f024f1097e3
Ericka76 <erickacarmella8@gmail.com>
2023-02-20 06:46:21 -0800

    lowercase

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
-#include <stdlib.h>                                             
-#include <time.h>                                               
-/* more headers goes there */                                   
-#include <stdio.h>                                              
-/* betty style doc for function main goes there */              
-/**                                                             
- * main - the alphabet in lowercase, followed by a new line
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ ** main - the alphabet in lowercase, followed by a new line
  *
- * Return: Always 0 (Success)
+ ** Return: Always 0 (Success)
  */
 int main(void)
 {
