bbfec45341dac09000d3a99be7ab63097a4f2c1b
Ericka76 <erickacarmella8@gmail.com>
2023-02-20 05:50:03 -0800

    lowercase

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -3,10 +3,13 @@
 int main(void) 
 {
 	char letter = 'a';
-	for (letter <= 'a'; letter <= 'z'; letter++) 
+
+	for (;letter <= 'a'; letter <= 'z'; letter++) 
 	{
 		putchar(letter);
 	}
+
 	putchar('\n');
+
 	return (0);
 }
