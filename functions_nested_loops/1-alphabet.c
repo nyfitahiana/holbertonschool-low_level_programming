5fd659aeb1e7ea54caccd185519bfeb38fc65fa1
Ericka76 <erickacarmella8@gmail.com>
2023-02-21 13:58:25 -0800

    lowercase

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-/**
  * print_alphabet - print all alphabet in lowercase
  */
 void print_alphabet(void)
@@ -20,4 +10,14 @@ void print_alphabet(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 	putchar(letter);
 	putchar('\n');
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
