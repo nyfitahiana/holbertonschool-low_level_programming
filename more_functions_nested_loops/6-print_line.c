171e17970c90602bb9d1fc09875755eafb7362de
Ericka76 <erickacarmella8@gmail.com>
2023-02-23 17:36:04 -0800

    printline

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_line - function that draws a straight line in the terminal
- * @n: number of times of the character
+ * @n: is the integer for the parameters of my function
  * Return: 0
  */
 void print_line(int n)
