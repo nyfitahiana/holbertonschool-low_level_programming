1a186c92adbc7341745c5315b9edb6abb6829598
Ericka76 <erickacarmella8@gmail.com>
2023-02-23 16:05:15 -0800

    most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - function that prints most of the numbers
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+	if (c != '2' && c != '4')
+	{
+	_putchar(c);
+	}
+	c++;
+	}
+_putchar('\n');
+}
