7d2e4cd305ae6efa041042baf02865ac6967dd0f
Ericka76 <erickacarmella8@gmail.com>
2023-02-27 10:48:29 -0800

    other char

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - Prints every other character of a string
+ * @str: the character to be printed
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
