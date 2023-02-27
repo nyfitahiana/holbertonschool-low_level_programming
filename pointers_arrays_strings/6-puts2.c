40ac9a4f5b9c83dd900616bd39541c12ed2384cf
Ericka76 <erickacarmella8@gmail.com>
2023-02-27 11:56:53 -0800

    other char

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -5,11 +5,19 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int len, i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
 	}
+
 	_putchar('\n');
 }
