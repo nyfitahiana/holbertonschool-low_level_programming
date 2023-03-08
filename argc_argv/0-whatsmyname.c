b9a97e574f5b87c8b708d1d9a032b6b222637f82
Ericka76 <erickacarmella8@gmail.com>
2023-03-08 15:54:43 -0800

    argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Prints the neme of a program
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings
+ * Return: Always 0 Success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
