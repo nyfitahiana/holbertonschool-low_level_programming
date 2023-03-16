0785539945ca69cd9830511d06295c03ab4456e7
Ericka76 <erickacarmella8@gmail.com>
2023-03-16 07:21:11 -0700

    main

---
+++ preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - prints the name of the file is was compiled from
+ *
+ * Return: Always 0;
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
