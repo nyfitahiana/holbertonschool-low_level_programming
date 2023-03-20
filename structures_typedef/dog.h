720ddae0fc67727b87a3958bda2f646bce28d36a
Ericka76 <erickacarmella8@gmail.com>
2023-03-20 06:35:53 -0700

    include

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,24 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dof
+ * @owner: name of the owner of the dog
+ *
+ */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+/* function prototypes */
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
