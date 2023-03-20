ee527e7d928007331c5856fcb0d411a02bef0d34
Ericka76 <erickacarmella8@gmail.com>
2023-03-20 06:42:07 -0700

    init

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-
-struct dog {
-	char *name;
-	float age;
-	char *owner;
-};
+#include "dog.h"
+/**
+ * init_dog - initialize struct dog.
+ * @d: name of the struct
+ * @name: name of dog in the struct
+ * @age: age of dog in struct
+ * @owner: name of owner of dog in struct
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d)
+	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	}
 }
