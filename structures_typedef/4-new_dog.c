be29a617a471eeafc67eefd0d4b9d5accf67df7a
Ericka76 <erickacarmella8@gmail.com>
2023-03-21 05:08:55 -0700

    free

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -46,3 +46,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (p);
 }
+
+/**
+ * free_dog - frees a dog structure
+ * @d: pointer to dog structure
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
